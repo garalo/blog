5766835237240caa41a46efb187b6c245a72b4a5
root <root@mail.myserver.com>
2011-11-08 12:25:18 +0900

    ilk defa ekleme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GaraloTk::Application.config.secret_token = '37551f29c8e4bc62cb05483a8c62cac6b6c657a1b67873c7629388d2b197fcc5560bcc58dcea275f9a0405deb7a50e5ff20d81f224124dc3be993cd327f4fadc'
